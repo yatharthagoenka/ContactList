1fdd2cf0d9b65eae67571370567c36f986eba797
Yathartha Goenka <goenkayathartha2002@gmail.com>
2023-03-03 02:26:40 +0530

    initiating backend

---
+++ backend/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/contactList')
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
